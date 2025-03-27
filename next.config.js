4bde3ac87c635284002d61806939c5d538d08c41
cheng <hmcbook@163.com>
2025-03-28 00:30:09 +0800

    去除图片优化

--- next.config.js
+++ next.config.js
@@ -6,4 +6,7 @@ module.exports = {
   reactStrictMode: true,
   output: 'export', // 生成静态文件到out目录
   poweredByHeader: false, // 移除X-Powered-By头
+  images: {
+    unoptimized: true // 禁用图片优化以支持静态导出
+  }
 } 
